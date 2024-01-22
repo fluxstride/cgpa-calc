488c4629cd5d32d9d05b3c634b70335a422ab258
Samuel Adekoya <samueladexx@gmail.com>
2024-01-22 15:02:04 +0100

    updated the result page

--- js/result.js
+++ js/result.js
@@ -1,11 +1,31 @@
 const gpaText = document.querySelector(".gpa");
+const gpaClassText = document.querySelector(".gpaClass");
+const restartButton = document.querySelector(".restart");
+
 const gpa = localStorage.getItem("GPA");
 
+const goBack = () => (window.location = "/");
+
 if (!gpa) {
-  window.history.back();
+  goBack();
 }
 
-gpaText.innerHTML = `Your GPA is ${gpa}`;
+let gpaClass = "";
+
+if (gpa >= 4.5) {
+  gpaClass = "First Class";
+} else if (gpa >= 3.5) {
+  gpaClass = "2nd Class Upper";
+} else if (gpa >= 2.5) {
+  gpaClass = "2nd Class Lower";
+} else {
+  gpaClass = "3rd Class Lower";
+}
+
+gpaText.setAttribute("data-gpa", gpa);
+gpaClassText.innerHTML = gpaClass;
+
+restartButton.addEventListener("click", goBack);
 
 window.addEventListener("beforeunload", () => {
   localStorage.removeItem("GPA");
